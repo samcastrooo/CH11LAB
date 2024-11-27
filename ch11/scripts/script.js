c5f1a19c3e2c1e451c833d4e45f2eb3a480b1c16
samcastrooo <156940634+samcastrooo@users.noreply.github.com>
2024-11-26 18:49:17 -0600

    Add files via upload

---
+++ ch11/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: firstName lastName
+    File Name: script.js
+    Date: 01/01/2020
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
